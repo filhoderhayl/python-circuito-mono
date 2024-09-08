e58762a0bea000d5c4deb2e8851d856da6878f8f
filhoderhayl <rhaylson.ribeiro1@gmail.com>
2024-09-07 23:09:17 -0300

    test1

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from Cargas import Circuito
 circuito = Circuito(220,60)
 print(circuito.cargas)
 print("\n"*2)
-circuito.adicionaCarga('P',15,0.85,True)
+circuito.adicionaCarga('P',3.5*0.7457,0.65,True)
+circuito.adicionaCarga('P',2.2,1,True)
+circuito.adicionaCarga('P',15*0.11,1,True)
+circuito.adicionaCarga('Q',2.4,0.75,False)
 print(circuito.cargas)
 print(abs(circuito.I)*1000)
 print(circuito.S)
-print("\n"*5)
-
-circuito.adicionaCarga('S',3,0.9,False)
-print(circuito.cargas)
-print(abs(circuito.I)*1000)
-print(circuito.S)
+#print("\n"*5)
 
 
